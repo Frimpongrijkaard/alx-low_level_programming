b3be43035dd87b99c8750254bdb4a6b1093419c9
Frimpong <frimpongrijkaard94@yahoo.com>
2022-10-17 10:59:26 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-/**
- * main -Entry point
- * 
- * Return: Always 0 (Success)
- */
+/*
+*main - this line of code print  using puts
+*
+*Return: Always 0
+*/
 int main(void)
-{
+{	
 	puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+	return(0);
 }
