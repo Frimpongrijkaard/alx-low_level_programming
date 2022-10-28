de94ca7c99d03434ab0259ac6460a5731f08fd63
Frimpong <frimpongrijkaard94@yahoo.com>
2022-10-27 20:22:47 -0700

    task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main"
+/**
+ * reverse_array- reverse ararray
+ * @a: number of a
+ * @n: number of size
+ *
+ * Return: 0
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < n; i++)
+	{
+		n--;
+		j = a[i];
+		a[i] = a[n];
+		a[n] = j;
+	}
+}
