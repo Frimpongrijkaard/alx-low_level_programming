59b3f013ba9891b4baec54cd920d6801560645e9
Frimpong <frimpongrijkaard94@yahoo.com>
2022-10-27 19:44:11 -0700

    task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -16,9 +16,9 @@ char *_strncpy(char *dest, char *src, int n)
 	while (i < n && dest[i] != '\0')
 	{
 		dest[i] = src[i];
-		++i;
+		i++;
 	}
-	for (i < n;  ++i)
+	for (i < n;  i++)
 	{
 		dest[i] != '\0';
 	}
