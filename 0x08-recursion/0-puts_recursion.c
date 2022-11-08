1f83422144785da773e0269b18921c3ebf5f1c48
frimpongrijkaard <frimpongrijkaard94@yahoo.com>
2022-11-08 06:07:35 -0800

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts_recursion- display the string
+ * followed by a new line
+ * @s: character string variable
+ *
+ * Return: no return
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+	}
+	else
+	{
+		_put_recursion(s + 1);
+	}
+	_putchar('\n');
+}
