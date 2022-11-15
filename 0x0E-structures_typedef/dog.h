06d41c3c79eda44fe855f393b78c5c988f0569a2
frimpongrijkaard <frimpongrijkaard94@yahoo.com>
2022-11-15 05:29:27 -0800

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+
+#define DOG_H
+
+/**
+ * struct dog - dog description
+ * @name: name of dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ */
+
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+#endif
