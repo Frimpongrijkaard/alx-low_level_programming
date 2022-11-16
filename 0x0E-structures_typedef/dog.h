67184c26f5baef0e1c9939c837047c16031f1997
frimpongrijkaard <frimpongrijkaard94@yahoo.com>
2022-11-15 17:15:03 -0800

    task 4

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,8 +19,12 @@ struct dog
 };
 
 /**
- * dog_t- typedef fot struct dog to dog_t
+ * dog_t- typedef for struct dog
  */
 typedef struct dog dog_t;
 
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
