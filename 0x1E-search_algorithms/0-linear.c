ccceab08f144d55c412d8b8d7b18aadd9c025bbb
Frimpongrijkaard <frimpongrijkaard94@yahoo.com>
2023-07-15 23:31:01 +0000

    solution of search algorithm

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,7 +12,7 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-	size_t n;
+	int n;
 
 	if (array == NULL)
 	{
@@ -22,7 +22,7 @@ int linear_search(int *array, size_t size, int value)
 	for (n = 0; n < size; n++)
 	{
 		printf("Value checked array[%u] = [%d]\n", n, array[n]);
-		if (array[n] == value)
+		if (value == array[n])
 		{
 			return (n);
 		}
