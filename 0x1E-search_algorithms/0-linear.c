0d11144107be0c9cc33ad946d9f60a12ce7657fe
Frimpongrijkaard <frimpongrijkaard94@yahoo.com>
2023-07-15 23:36:57 +0000

    solution of search algorithm

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -21,10 +21,10 @@ int linear_search(int *array, size_t size, int value)
 
 	for (i = 0; i < size; i++)
 	{
-		printf("Value checked array[%li] = [%i]\n", x, array[i]);
+		printf("Value checked array[%li] = [%i]\n", i, array[i]);
 		if (array[i] == value)
 		{
-			return (x);
+			return (i);
 		}
 	}
 	return (-1);
